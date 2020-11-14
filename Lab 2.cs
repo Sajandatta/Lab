d31cbcb04f38caf039010e2c2efba8bb54acb823
sajan datta <sajandatta1@gmail.com>
2020-11-14 11:29:19 +0600

    my first commi

---
+++ Lab 2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace lab_atsk_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            static void Main(string[] args)
+            { //N-1 1 
+
+                Console.WriteLine("Hello");
+                Console.WriteLine("Sajan");
+                Console.WriteLine();
+                //N- 2
+                int x = 5, y = 7;
+                Console.WriteLine(x);
+                Console.WriteLine(y);
+                int sum = x + y;
+                Console.WriteLine("Sum of two number is " + sum);
+                Console.WriteLine();
+                //Num 3 
+                int a = 15, b = 3;
+                int div = (a / b);
+                Console.WriteLine(div);
+                Console.WriteLine();
+                //N-4
+                Console.WriteLine("Answer of number a." + -5 + 8 * 6);
+                Console.WriteLine("Answer of number b." + (55 + 9) % 9);
+                Console.WriteLine("Answer of number c." + 20 + (-3 * 5) / 8);
+                //N-5 5 
+                int number1 = 4, number2 = 3;
+                int sumation = number1 + number2;
+                int multiply = (number1 * number2);
+                int subtraction = number1 - number2;
+                int divide = number1 / number2;
+                float reminder = number1 % number2;
+                Console.WriteLine(sumation);
+                Console.WriteLine(multiply);
+                Console.WriteLine(subtraction);
+                Console.WriteLine(divide);
+                Console.WriteLine(reminder);
+                //number 6
+                Console.WriteLine((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+                //N- 7 
+                double width = 5.5, height = 8.5;
+                double area = width * height;
+                Console.WriteLine(area);
+                double perimeter = 2 * (width + height);
+                Console.WriteLine(perimeter);
+            }
+        }
+    }
---
+++ lab3...cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    public class Student
+    {
+        private String name;
+        private String id;
+        private String department;
+        private Book[] borrowedBook;
+        private int borrowedBookCount;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+      
